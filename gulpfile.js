17dd14e0ecbe1a2168964a395643989eeab42017
chaoshuai <chao.shuai@qq.com>
2017-10-08 22:13:09 +0800

    feat -> 添加开源项目

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,29 @@ var runSequence = require('run-sequence');
 
 
 /*************************开发模式*************************/
+gulp.task('server:dev', function() {
+	browserSync.init({
+		server: {
+			baseDir: './src/',
+			index: 'index.html'
+		},
+		port: 8080
+	})
+	gulp.watch('src/css/*.css', ['css:dev'])
+	gulp.watch('src/js/*.js', ['js:dev'])
+	gulp.watch('src/*.html', ['html:dev'])
+})
 
+gulp.task('html:dev' , function(){
+	gulp.src([
+		'./src/*.html'
+	])
+	.pipe(gulp.dest('./src/'))
+	.pipe(reload({
+		stream:true
+	}))
+
+});
 gulp.task('css:dev', function () {
 	gulp.src(['src/css/*.css','!src/css/all.css','!src/css/all.min.css'])
     .pipe(autoprefixer())
@@ -32,6 +54,9 @@ gulp.task('css:dev', function () {
     .pipe(cleanCss())
     .pipe(rename('all.min.css'))
     .pipe(gulp.dest('src/css/'))
+	.pipe(reload({
+		stream:true
+	}))
 })
 gulp.task('js:dev',function () {
     gulp.src(['src/js/*.js','!src/js/all.js','!src/js/all.min.js'])
@@ -42,6 +67,9 @@ gulp.task('js:dev',function () {
     .pipe(uglify())
     .pipe(rename('./all.min.js'))
     .pipe(gulp.dest('src/js/'))
+	.pipe(reload({
+		stream:true
+	}))
 })
 /*************************生产模式*************************/
 gulp.task('html:pro', function () {
